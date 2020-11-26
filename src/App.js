3efecd172092fe0c2e122c6986dcc107646691df
toot09 <03toot28@gmail.com>
2020-11-26 22:14:29 +0900

    testing function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import Ujone from './Ujone';
+//import Ujone from './Ujone';
+
+function Food({name, price}) {
+  //console.log(props);
+  //alert(props.age);
+  return (
+  //<h1> Ma age is {props.age}</h1>
+  <h1> Ma favorite foods is {name} and price is {price}</h1>
+  )
+}
+
+const foodILike = [
+  {name : "samgyupsal",
+  price : 12000},
+  {name : "kimbab",
+  price : 2000},
+  {name : "gukbab",
+  price : 8000}
+]
 
 function App() {
   return (
     <div>
       <h1>Helloo!!!</h1>
-      <Ujone/>
+      {foodILike.map(dish=><Food name={dish.name} price={dish.price}/>)}
     </div>
   );
 }
