66b601ad988f6ecb297713eedc3e9ac4800a2236
toot09 <03toot28@gmail.com>
2020-12-01 21:21:20 +0900

    Longtime no commit!

--- src/App.js
+++ src/App.js
@@ -1,31 +1,56 @@
 import React from 'react';
+import propTypes from "prop-types";
 //import Ujone from './Ujone';
 
-function Food({name, price}) {
+function Food({name, price, rating}) {
   //console.log(props);
   //alert(props.age);
   return (
   //<h1> Ma age is {props.age}</h1>
-  <h1> Ma favorite foods is {name} and price is {price}</h1>
+  <div>
+    <h1>My favorite foods is {name}</h1>
+    <h2>price : {price}</h2>
+    <h3>rating : {rating}/5.0</h3>
+  </div>
   )
 }
 
 const foodILike = [
-  {name : "samgyupsal",
-  price : 12000},
-  {name : "kimbab",
-  price : 2000},
-  {name : "gukbab",
-  price : 8000}
+  {key : 1,
+  name : "samgyupsal",
+  price : 12000,
+  rating : 5.0},
+  {key : 2,
+  name : "kimbab",
+  price : 2000,
+  rating : 2.0},
+  {key : 3,
+  name : "gukbab",
+  price : 8000,
+  rating : 3.8}
 ]
 
+Food.propTypes = {
+  name : propTypes.string.isRequired,
+  price : propTypes.number.isRequired,
+  rating : propTypes.number.isRequired
+}
+
+function Dishes(dish) {
+  //console.log(dish.name);
+  return <Food key={dish.key} name={dish.name} price={dish.price} rating={dish.rating} />
+}
+
 function App() {
+return <div>{foodILike.map(Dishes)}</div>
+  /*
   return (
     <div>
       <h1>Helloo!!!</h1>
       {foodILike.map(dish=><Food name={dish.name} price={dish.price}/>)}
     </div>
   );
+  */
 }
 
 export default App;
